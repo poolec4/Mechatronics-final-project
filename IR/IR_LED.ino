e250829edcc604321aae53ed3db6955567aeb30f
Scott Barnes <scottgbarnes@gwu.edu>
2018-02-28 16:12:15 -0500

    Added LED Code

---
+++ IR/IR_LED.ino
@@ -0,0 +1,32 @@
+double f2 = 10;
+
+double T2 = 1.0/f2;
+
+unsigned long t_old1;
+unsigned long t_old;
+
+boolean bool2 = false;
+
+int outPin = 7;
+
+void setup() {
+  Serial.begin(9600);
+  t_old = micros();
+  pinMode(outPin, OUTPUT);
+  Serial.println(f2);
+  Serial.println(T2);
+  
+}
+
+void loop() {
+  if (abs(micros()-t_old) >= T2*1000000.0/2.0){
+    bool2 = !(bool2);
+    if (bool2 == true){
+      tone(10, 38000);
+    }
+    else{
+      noTone(10);
+    }
+    t_old = micros();
+  }
+}
