992b586b3cfa1e407a26b6d30c93f50c659df64b
Chris Poole <poolec4@aol.com>
2018-03-23 16:21:40 -0400

    Added motor test

---
+++ Main/motor_test/motor_test.ino
@@ -0,0 +1,19 @@
+#include <Servo.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  Servo L_servo;  // create servo object to control a servo
+  Servo R_servo;  // create servo object to control a servo
+  
+  L_servo.attach(2);  // attaches the servo on pin 9 to the servo object
+  R_servo.attach(3);  // attaches the servo on pin 9 to the servo object
+
+  L_servo.write(92);              
+  R_servo.write(92);              
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
