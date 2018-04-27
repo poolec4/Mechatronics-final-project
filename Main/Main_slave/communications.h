79f3f1237b69fa599b0988bd10ee18456a4ea8ff
Chris Poole <poolec4@aol.com>
2018-04-27 10:37:08 -0400

    Added tag available function

--- Main/Main_slave/communications.h
+++ Main/Main_slave/communications.h
@@ -17,6 +17,36 @@ void add_int_to_string(char *string, int value, char const *tag, bool last_value
     }
 }
 
+bool is_tag_available(char *string, char const *tag)
+{
+  char* string_copy = (char*)malloc(strlen(string) + 1);
+  char* char_result;
+  char* token;
+  bool available = false;
+
+  strcpy(string_copy, string);
+  token = strtok(string_copy, "&");
+
+  while(token)
+  {
+    char* equals_sign = strchr(token, '=');
+
+    if(equals_sign)
+    {
+      *equals_sign = 0;
+
+      if(0 == strcmp(token, tag))
+      {
+	available = true;
+      }
+    }
+    token = strtok(0, "&");
+  }
+  free(string_copy);
+
+  return available;
+}
+
 int parse_string_to_int(char *string, char const *tag)
 {
   char* string_copy = (char*)malloc(strlen(string) + 1);
